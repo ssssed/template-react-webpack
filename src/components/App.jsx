40eee690adc0a054248e8139ff4c25d5d3601f69
Igor Koneshov <igorkoneshov@mail.ru>
2022-07-18 21:24:45 +0400

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React worked!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.jsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  devServer: {
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpeg|gif||jpg|woff(2)?|eot|ttf|otf|svg)$/i,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
