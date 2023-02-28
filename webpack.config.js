3507a58cb1c40d4456de879d318b061bb18bc5fd
Igor Koneshov <igorkoneshov@mail.ru>
2023-03-01 01:21:19 +0400

    feat: refactor and update package

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,94 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: ['@babel/polyfill', './src/index.jsx'],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    port: 3000,
-    static: './development',
-    historyApiFallback: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, './src')
+module.exports = (env, args) => {
+  const envFile = path.resolve(__dirname, `.env.${args.mode}`);
+  return {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.jsx'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
     },
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: path.resolve("src",
-          "assets", "favicon.png")
-    }),
-    new FaviconsWebpackPlugin({
-          logo: path.resolve("src", "assets", "favicon.png"),
-          inject: htmlPlugin =>
-              path.basename(htmlPlugin.options.filename) === 'index.html',
-        }
-    ),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(scss|sass|css)$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+    devServer: {
+      port: 3000,
+      static: './development',
+      historyApiFallback: true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-      {
-        test: /\.(png|jpeg|gif||jpg|woff(2)?|eot|ttf|otf|svg)$/i,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(jsx|js)?$/,
-        exclude: /node-modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+      }),
+      new DotenvWebpackPlugin({
+        path: envFile,
+        safe: true,
+        systemvars: true,
+        silent: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[chunkhash].css',
+        chunkFilename: '[id].[chunkhash].css',
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '/',
+              },
+            },
+            'css-loader',
+            'sass-loader',
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpeg|gif|ico|jpg|woff(2)?|eot|ttf|otf|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(jsx|js)?$/,
+          exclude: /node-modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
